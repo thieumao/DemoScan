4dfdc6e9fb0d168c490f7352dd20fa0ef86767db
Thieu Nguyen <thieu@bluegrassdigital.com>
2020-11-17 18:01:19 +0700

    Temp

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   SafeAreaView,
   StyleSheet,
@@ -6,6 +6,7 @@ import {
   View,
   Text,
   StatusBar,
+  Image,
 } from 'react-native';
 
 import {
@@ -18,11 +19,35 @@ import {
 import DocumentScanner from "@woonivers/react-native-document-scanner"
 
 const App = () => {
+  // image: data.croppedImage,
+  //             initialImage: data.initialImage,
+  //             rectangleCoordinates: data.rectangleCoordinates
+  const [image, setImage] = useState('');
+
   return (
     <>
       <StatusBar barStyle="dark-content" />
       <SafeAreaView>
         <View style={styles.container}>
+        <DocumentScanner
+          useBase64
+          saveInAppDocument={false}
+          onPictureTaken={data => setState(data.croppedImage)}
+          overlayColor="rgba(255,130,0, 0.7)"
+          enableTorch={false}
+          brightness={0.3}
+          saturation={1}
+          contrast={1.1}
+          quality={0.5}
+          onRectangleDetect={({ stableCounter, lastDetectionType }) => null}
+          detectionCountBeforeCapture={5}
+          detectionRefreshRateInMS={50}
+          onPermissionsDenied={() => console.log("Permissions Denied")}
+        />
+        <Image
+          source={{ uri: `data:image/jpeg;base64,${image}` }}
+          resizeMode="contain"
+        />
         </View>
       </SafeAreaView>
     </>
